e5c811a3bf3f6e4ff8124a36abe5f706d03acc8c
elladiem <ella.diem@students.fhnw.ch>
2022-10-07 15:05:48 +0200

    Einlese-Datei_eingefügt_und_Code-Datei_einlesen_geschrieben

--- src/projekt_jabberwocky/Main.java
+++ src/projekt_jabberwocky/Main.java
@@ -1,10 +1,24 @@
 package projekt_jabberwocky;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// Test 1.0.0.1 Veronika Hübscher
+		// Datei Objekt erstellen (Ella)
+		File datei = new File("Text.txt");
 
+		// Text aus datei auslesen
+		Scanner scan = null;
+		try {
+			scan = new Scanner(datei);
+		} catch(FileNotFoundException e){
+			System.out.println("File not found!");
+		}
+		while (scan.hasNext()) {
+			System.out.println(scan.nextLine());
+		}
 	}
 
 }
