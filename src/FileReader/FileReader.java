072298a1453c26a0886da7d0fbe0278b0a76109d
veron <veron@DESKTOP-CTVNEH8>
2022-10-10 15:26:25 +0200

    File eingelesen

---
+++ src/FileReader/FileReader.java
@@ -0,0 +1,28 @@
+package FileReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReader {
+
+	public static void main(String[] args) {
+		
+	File datei = new File("Gedicht.txt");
+	
+	//Überprüfen ob die Datei vorhanden ist, es muss True rauskommen
+	//System.out.println(datei.isFile());
+	
+	//Datei einlesen 
+	Scanner scan = null; 
+	try {
+		scan = new Scanner(datei);
+	} catch (FileNotFoundException e) {
+		System.out.println("File Not Found");
+	}
+	while(scan.hasNext()) {
+	System.out.println(scan.nextLine());
+	}
+	}
+
+}
